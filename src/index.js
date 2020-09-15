327325d70ae6c28066b5ffaf5f2a6350ea690c38
Cat <ormallen@gmail.com>
2020-09-15 23:44:42 +0300

    feat: implement reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-module.exports = function reverse (n) {
-  
+ module.exports = function reverse (n) {
+  n = Math.abs(n);
+  let out;
+  n == 0 ? out = [0] : out = [];
+  while (n!=0){
+    out.push(n % 10);
+    n = Math.floor(n / 10);
+  }
+  return Number(out.join(""));
 }
+
+/* console.log(reverse(162));
+console.log(reverse(-192));
+console.log(reverse(0));
+console.log(reverse(100)); */
